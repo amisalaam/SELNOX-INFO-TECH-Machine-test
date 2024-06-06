3ead8db903da2ef0697ec43ad58c00ba28bc0d9f
amisalaam <amisalaam9@gmail.com>
2024-06-06 18:29:21 +0530

    Created modals according to the task sheet

--- vendor/settings.py
+++ vendor/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #Created apps
     'vendorApp',
+    
+    #Installed apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
